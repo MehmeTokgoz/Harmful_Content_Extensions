fdb426685afcb9cf0c465b599498c1e447e71691
MEHMETTOKGOZ <42mt06@gmail.com>
2023-05-26 00:07:03 +0300

    ShowDetails and Turn back completed

--- src/components/UsomDatas/HarmfulContentTable.jsx
+++ src/components/UsomDatas/HarmfulContentTable.jsx
@@ -104,38 +104,73 @@ function HarmfulContentTable() {
     return stabilizedThis.map((el) => el[0]);
   }
 
-  const headCells = [
-    {
-      id: "name",
-      numeric: false,
-      disablePadding: true,
-      label: "Zararlı Adres (Url)",
-    },
-    {
-      id: "date",
-      numeric: true,
-      disablePadding: false,
-      label: "Eklenme Tarihi",
-    },
-    {
-      id: "criticality_level",
-      numeric: true,
-      disablePadding: false,
-      label: "Kritiklik Seviyesi",
-    },
-    {
-      id: "description",
-      numeric: true,
-      disablePadding: false,
-      label: "Açıklama",
-    },
-    {
-      id: "source",
-      numeric: true,
-      disablePadding: false,
-      label: "Kaynak",
-    },
-  ];
+  const headCells = showDetails
+    ? [
+        {
+          id: "domain_name",
+          numeric: false,
+          disablePadding: true,
+          label: "Domain İsmi",
+        },
+        {
+          id: "city",
+          numeric: true,
+          disablePadding: false,
+          label: "Şehir",
+        },
+        {
+          id: "country",
+          numeric: true,
+          disablePadding: false,
+          label: "Ülke",
+        },
+        {
+          id: "zip_code",
+          numeric: true,
+          disablePadding: false,
+          label: "Posta Kodu",
+        },
+        {
+          id: "phone",
+          numeric: true,
+          disablePadding: false,
+          label: "Telefon",
+        },
+        {},
+      ]
+    : [
+        {
+          id: "name",
+          numeric: false,
+          disablePadding: true,
+          label: "Zararlı Adres (Url)",
+        },
+        {
+          id: "date",
+          numeric: true,
+          disablePadding: false,
+          label: "Eklenme Tarihi",
+        },
+        {
+          id: "criticality_level",
+          numeric: true,
+          disablePadding: false,
+          label: "Kritiklik Seviyesi",
+        },
+        {
+          id: "description",
+          numeric: true,
+          disablePadding: false,
+          label: "Açıklama",
+        },
+        {
+          id: "source",
+          numeric: true,
+          disablePadding: false,
+          label: "Kaynak",
+        },
+        {},
+      ];
 
   function EnhancedTableHead(props) {
     const {
@@ -219,7 +254,7 @@ function HarmfulContentTable() {
           }),
         }}
       >
-        {numSelected > 0 ? (
+        {numSelected > 0 && !showDetails ? (
           <Typography
             sx={{ flex: "1 1 100%" }}
             color="inherit"
@@ -227,6 +262,15 @@ function HarmfulContentTable() {
             component="div"
           >
             {numSelected} Selected
+          </Typography>
+        ) : showDetails ? (
+          <Typography
+            sx={{ flex: "1 1 100%" }}
+            variant="h6"
+            id="tableTitle"
+            component="div"
+          >
+            DETAYLAR
           </Typography>
         ) : (
           <Typography
@@ -315,18 +359,17 @@ function HarmfulContentTable() {
 
   const handleShowInfo = async (url) => {
     try {
-      setShowDetails(true);
       const response = await axios.get(
         `/api?key=0607942437A13C55233425498F4F2AFD&domain=${url}`
       );
       const data = response.data;
       setDetails([data]);
+      setShowDetails(true);
     } catch (error) {
       console.log(error);
     }
   };
-
-  console.log(details.map((item)=>item.registrant.phone))
+  console.log(details.map((item) => item.registrant));
 
   return (
     <Box className="main-table-container">
@@ -353,67 +396,81 @@ function HarmfulContentTable() {
                     const isItemSelected = isSelected(detail.registrant.name);
                     const labelId = `enhanced-table-checkbox-${index}`;
                     return (
-                      <TableRow
-                        hover
-                        onClick={(event) =>
-                          handleClick(event, detail.registrant.name)
-                        }
-                        role="checkbox"
-                        aria-checked={isItemSelected}
-                        tabIndex={-1}
-                        key={detail.billing.domain_id}
-                        selected={isItemSelected}
-                        sx={{ cursor: "pointer" }}
-                        className={`harmful-content-table-row ${
-                          isItemSelected
-                            ? "harmful-content-table-row-selected"
-                            : ""
-                        }`}
-                      >
-                        <TableCell padding="checkbox">
-                          <Checkbox
-                            color="primary"
-                            checked={isItemSelected}
-                            inputProps={{
-                              "aria-labelledby": labelId,
-                            }}
+                      <>
+                        <TableRow
+                          hover
+                          onClick={(event) =>
+                            handleClick(event, detail.registrant.name)
+                          }
+                          role="checkbox"
+                          aria-checked={isItemSelected}
+                          tabIndex={-1}
+                          key={detail.billing.domain_id}
+                          selected={isItemSelected}
+                          sx={{ cursor: "pointer" }}
+                          className={`harmful-content-table-row ${
+                            isItemSelected
+                              ? "harmful-content-table-row-selected"
+                              : ""
+                          }`}
+                        >
+                          <TableCell padding="checkbox">
+                            <Checkbox
+                              color="primary"
+                              checked={isItemSelected}
+                              inputProps={{
+                                "aria-labelledby": labelId,
+                              }}
+                              className="harmful-content-table-body-cell"
+                            />
+                          </TableCell>
+                          <TableCell
+                            component="th"
+                            id={labelId}
+                            scope="row"
+                            padding="none"
                             className="harmful-content-table-body-cell"
-                          />
-                        </TableCell>
-                        <TableCell
-                          component="th"
-                          id={labelId}
-                          scope="row"
-                          padding="none"
-                          className="harmful-content-table-body-cell"
-                        >
-                          {detail.billing.domain}
-                        </TableCell>
-                        <TableCell
-                          align="right"
-                          className="harmful-content-table-body-cell"
-                        >
-                          {detail.registrant.city}
-                        </TableCell>
-                        <TableCell
-                          className="harmful-content-table-body-cell"
-                          align="right"
-                        >
-                          {detail.registrant.country}
-                        </TableCell>
-                        <TableCell
-                          className="harmful-content-table-body-cell"
-                          align="right"
-                        >
-                          {detail.registrant.email}
-                        </TableCell>
-                        <TableCell
-                          className="harmful-content-table-body-cell"
-                          align="right"
-                        >
-                          {detail.registrant.phone}
-                        </TableCell>
-                      </TableRow>
+                          >
+                            {detail.registrant.name}
+                          </TableCell>
+                          <TableCell
+                            align="right"
+                            className="harmful-content-table-body-cell"
+                          >
+                            {detail.registrant.city}
+                          </TableCell>
+                          <TableCell
+                            className="harmful-content-table-body-cell"
+                            align="right"
+                          >
+                            {detail.registrant.country}
+                          </TableCell>
+                          <TableCell
+                            className="harmful-content-table-body-cell"
+                            align="right"
+                          >
+                            {detail.registrant.zip_code}
+                          </TableCell>
+                          <TableCell
+                            className="harmful-content-table-body-cell"
+                            align="right"
+                          >
+                            {detail.registrant.phone}
+                          </TableCell>
+                          <TableCell
+                            className="harmful-content-table-body-cell"
+                            align="right"
+                          >
+                            <Button
+                              variant="outlined"
+                              onClick={() => setShowDetails(false)}
+                              id="button-show-info"
+                            >
+                              GERİ DÖN
+                            </Button>
+                          </TableCell>
+                        </TableRow>
+                      </>
                     );
                   })
                 : visibleRows.map((row, index) => {
