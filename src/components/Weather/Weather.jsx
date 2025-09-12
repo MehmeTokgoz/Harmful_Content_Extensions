b7b60c3b5378c8458e06da5c02c0aa8f425b50a4
MEHMETTOKGOZ <42mt06@gmail.com>
2025-09-12 23:37:36 +0200

    soic-quote need to be fixed

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -9,10 +9,10 @@ function Weather() {
   const getWeather = async () => {
     try {
       const key = import.meta.env.VITE_API_KEY;
-      console.log("API KEY:", key) // Vercel için VITE_ prefix önerilir
+      
       const url = `https://api.openweathermap.org/data/2.5/weather?q=Paris&units=metric&appid=${key}`;
       const { data } = await axios.get(url);
-      console.log("Hava durumu:", data)
+      
       setWeatherInfo(data);
     } catch (error) {
       console.error("Error fetching weather:", error);
