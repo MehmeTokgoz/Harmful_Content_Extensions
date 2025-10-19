ef73bc9190ef2e494641cae5d1fe35c815d8f4ba
MEHMETTOKGOZ <42mt06@gmail.com>
2025-10-19 21:55:26 +0200

    old version was changed

--- src/components/DateAndClock/DateAndClock.jsx
+++ src/components/DateAndClock/DateAndClock.jsx
@@ -27,32 +27,3 @@ function DateAndClock() {
 }
 
 export default DateAndClock;
-
-{/** 
-// eslint-disable-next-line no-unused-vars
-import React, { useEffect, useState } from "react";
-import "./DateAndClock.scss";
-
-function DateAndClock() {
-  const [currentDate, setCurrentDate] = useState(new Date());
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentDate(new Date());
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  return (
-    <div className="date-clock-container">
-      <p className="date-info">Date: {currentDate.toLocaleDateString("tr-TR")}</p>
-      <p className="clock-info">Clock: {currentDate.toLocaleTimeString()}</p>
-    </div>
-  );
-}
-
-export default DateAndClock;
-*/}
